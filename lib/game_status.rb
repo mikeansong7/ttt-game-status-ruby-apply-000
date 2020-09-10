d0bfa9359b8ff4f0c48f5f33ca383066542b99cd
Michael B Ansong <michael.ansong7@gmail.com>
2020-09-10 21:29:12 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -37,5 +37,13 @@ def full?(board)
   board.all? {|index| index == "X" || index == "O"}
 end
 
+def draw?(board)
+  if !won?(board) && full?(board)
+    return true
+  else
+    return false
+  end
+end
+
 
 
